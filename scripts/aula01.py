2e0c08550d0c6e27937e62e845be82d9a861d86c
Pedro Silva - Pezog <30021063+PedrBandeira@users.noreply.github.com>
2023-08-14 20:33:16 -0300

    Create aula01.py

---
+++ scripts/aula01.py
@@ -0,0 +1,5 @@
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+peso = input('Qual o seu peso? ')
+
+print(f"Meu nome é {nome} e eu tenho {idade} anos. Estou pesando {peso}kg")
