bd5416f5fd5e56db994c3463bcef69eac666bd58
Pedro Silva - Pezog <30021063+PedrBandeira@users.noreply.github.com>
2023-08-15 14:47:20 -0300

    Finalização do ex024

--- scripts/ex024.py
+++ scripts/ex024.py
@@ -1,14 +1,5 @@
 # Faça um programa que abra e reproduza o áudio de um arquivo mp3
 import pygame
-
-# Inicia o módulo de áudio
 pygame.init()
-
-# Carrega o arquivo MP3
 pygame.mixer.music.load('../audio/audio.mp3')
-
-# Inicia a reprodução do arquivo
 pygame.mixer.music.play()
-
-# Mantém o programa em execução enquanto o áudio está sendo reproduzido
-pygame.event.wait()
